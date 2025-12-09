c4f201207525dbd6c68cf2b85abbae2a419ccfc6
Gordius-Tech <160427572+Gordius-Tech@users.noreply.github.com>
2025-12-09 17:26:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const signs = ['\u2648','\u2649','\u264A','\u264B','\u264C','\u264D','\u264E','\u264F','\u2650','\u2651','\u2652','\u2653'];
-const total = 100;
+const total = 99;
 const perRow = 15;
 const container = document.getElementById('columns-container');
 
@@ -49,4 +49,3 @@ btn.addEventListener('click', function() {
   resetBtnContainer.appendChild(resetBtn);
   mainContent.appendChild(resetBtnContainer); // A reset gomb hozzáadása a main-content panelhez
 });
-
